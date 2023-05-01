2264d62f7e88b0b9cbd12b30df84c83e75320fb3
Julien Mereau <jumereau62223@gmail.com>
2023-05-01 12:37:49 +0200

    ptn toujours j'oublie qqch

--- src/gan/sampler.py
+++ src/gan/sampler.py
@@ -11,7 +11,8 @@ class EulerMaruyamaSampler():
         score_model = None, 
         corrector: bool = False,
         shape: list = [1, 1, 28, 28],
-        eps: float = 1e-3
+        eps: float = 1e-3,
+        device: float = 'cuda'
     ) -> None:
         self.sde = sde
         # self.rsde = sde.reverse(score_model)
@@ -19,6 +20,7 @@ class EulerMaruyamaSampler():
         self.corrector = corrector
         self.shape = shape
         self.eps = eps
+        self.device = device
 
     def sampling(self, save_evolution: bool = False) -> torch.Tensor:
 
@@ -29,14 +31,14 @@ class EulerMaruyamaSampler():
         if save_evolution:
             sampler_evol = []
 
-        init_x = self.sde.prior_sampling(self.shape).to(device)
-        time_steps = torch.linspace(1., self.eps, self.sde.N, device=device)
+        init_x = self.sde.prior_sampling(self.shape).to(self.device)
+        time_steps = torch.linspace(1., self.eps, self.sde.N, device=self.device)
         step_size = time_steps[0] - time_steps[1]
         x = init_x
         with torch.no_grad():
           for time_step in ntqdm(time_steps):
-            x = x.to(device)
-            batch_time_step = torch.ones(x.shape[0], device=device) * time_step
+            x = x.to(self.device)
+            batch_time_step = torch.ones(x.shape[0], device=self.device) * time_step
             f, g = self.sde.sde(x, batch_time_step)
             mean_x = x + (g**2)[:, None, None, None] * self.score_model(x, batch_time_step) * step_size
             x = mean_x + torch.sqrt(step_size) * g[:, None, None, None] * torch.randn_like(x)
